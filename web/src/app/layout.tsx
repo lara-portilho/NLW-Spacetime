a1262a9a720c25ff1bd573ee75a7fc5f678eacb4
Lara Portilho <lara.portilho.m@gmail.com>
2023-05-15 22:31:04 -0300

    ✨ Adicionado configuração da web

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Inter } from 'next/font/google'
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'NLW - Spacetime',
+  description: 'Next Level Week - Spacetime',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
